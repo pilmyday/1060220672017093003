9f5ef0f141ea30da1715e55282082696b8aae58f
pilmyday <roy00374@mail.com>
2017-09-30 15:44:15 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x;
+        System.out.print("輸入攝氏=");
+        x=in.nextInt();
+	    System.out.printf("換算華氏=%d\n",x*9/5+32);
+
+    }
+}
